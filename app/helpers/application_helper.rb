a58b2570d032b62d01d46973cc2421e96ce2a85d
Jason McInerney <jason.j.mcn@gmail.com>
2011-05-02 22:47:18 -0700

    changed title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Return a title on a per-page basis.
   def title
-    base_title = "McInerney Educational Consulting"
+    base_title = "Jennifer Scott Educational Consulting"
     if @title.nil?
       base_title
     else
